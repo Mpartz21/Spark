3d4eab929b7300b408afcb70b3033a9650bd6400
Miguel Partida <mig21.ap@gmail.com>
2021-06-08 00:31:52 -0700

    Week-3 Labs

---
+++ Week-3/Lab-Datatypes/src/VariableDeclaration.java
@@ -0,0 +1,27 @@
+
+public class VariableDeclaration {
+	
+	public static void main(String[] args){
+		boolean trueOrFalse = false;
+	    System.out.println(trueOrFalse);
+	    
+	    short shortie = 16;
+	    System.out.println(shortie);
+	    
+	    int number = -32;
+	    System.out.println(number);
+	    
+	    float floaties = 3.1411111F;
+	    System.out.println(floaties);
+	    
+	    long reallyLong = 1234567890L;
+	    System.out.println(reallyLong);
+	    
+	    char c = 'w';
+	    System.out.println(c);
+	    
+	
+	    
+	}
+	
+}
